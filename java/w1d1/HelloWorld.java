be4e3c5c186ecc7202746068edf9bc6ff49042cd
Brendan Stanton <bmsenate@gmail.com>
2021-04-05 11:12:07 -0700

    add monday

---
+++ java/w1d1/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!! This is my first java app!!");
+        System.out.println("This is also my new java app!");
+
+        int age = 10;
+        String name = "Brendan";
+        char singleChar = 'b';
+        boolean isFalse = true;
+        // type identifier value
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        boolean[] votes = {true, false};
+
+        OtherFile myVariableName = new OtherFile();
+        myVariableName.sayStuff();
+    }
+}
+
+
+
---
+++ java/w1d1/OtherFile.java
@@ -0,0 +1,5 @@
+public class OtherFile {
+    public static void sayStuff(){
+        System.out.println("I am other file and i say stuff!");
+    }
+}
